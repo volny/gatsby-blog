a85be909bad0dc656cc79f5b82238dfd4e795eba
Felix Volny <felix@volny.co>
2017-09-27 18:08:54 +0700

    global typography config

--- src/pages/counter.js
+++ src/pages/counter.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'gatsby-link'
 
 class Counter extends React.Component {
   constructor () {
@@ -9,15 +10,20 @@ class Counter extends React.Component {
   render () {
     return (
       <div style={{ right: '50%', bottom: '50%', transform: 'translate(50%,50%)', position: 'absolute' }}>
-      <h1>Counter</h1>
-      <p>current count: {this.state.count}</p>
-      <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-        plus
-      </button>
-      <button onClick={() => this.setState({ count: this.state.count - 1 })}>
-        minus
-      </button>
-    </div>
+        <h1>Counter</h1>
+        <p>current count: {this.state.count}</p>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          plus
+        </button>
+        <button onClick={() => this.setState({ count: this.state.count - 1 })}>
+          minus
+        </button>
+        <div style={{ padding: '1rem 0' }}>
+          <Link to="/">Home</Link>
+          <span> / </span>
+          <Link to="/page-2/">Page 2</Link>
+        </div>
+      </div>
     )
   }
 }
