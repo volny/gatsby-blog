e097d53a183a1148b12457d5b5fe6a53e39c8480
Felix Volny <felix@volny.co>
2017-09-29 22:26:50 +0700

    showing files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,54 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-export default () =>
-  <div>
-    <h1>Hello</h1>
-    <p>This is Blog.</p>
-    <p>Read <Link to="/post-1/">Blog Post</Link></p>
-    <p>Marvel at <Link to="/counter/">Counter</Link></p>
-    <img src="http://lorempixel.com/400/200/" alt="" />
-    <p>
-    </p>
-  </div>
+export default ({ data }) => {
+  console.log(data)
+  return (
+      <div>
+        <h1>Files</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>relativePath</th>
+              <th>prettySize</th>
+              <th>extension</th>
+              <th>birthTime</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.allFile.edges.map(({ node }) =>
+              <tr>
+                <td>
+                  {node.relativePath}
+                </td>
+                <td>
+                  {node.prettySize}
+                </td>
+                <td>
+                  {node.extension}
+                </td>
+                <td>
+                  {node.birthTime}
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+
+export const query = graphql`
+  query MyFilesQuery {
+    allFile {
+      edges {
+        node {
+          relativePath
+          prettySize
+          extension
+          birthTime(fromNow: true)
+        }
+      }
+    }
+  }
+`
