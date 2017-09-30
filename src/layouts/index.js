65919563103f143073bfc8c0070dd89bd2536446
Felix Volny <felix@volny.co>
2017-09-30 07:59:06 +0700

    footer layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -12,6 +12,31 @@ const HeaderLink = styled(Link)`
   }
 `
 
+const Footer = styled.footer`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-content: space-around;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100vw;
+  height: 80px;
+  background-color: rgba(150, 150, 150, 0.5);
+  padding: 0 15%;
+  @media (max-width: 700px) {
+    padding: 0 5%;
+  }
+`
+
+const FooterItem = styled.span`
+  margin: auto;
+  color: #ffffff;
+  &:hover {
+    color: tomato;
+  }
+`
+
 const ListLink = ({ to, children }) =>
   <li style={{
     display: `inline-block`,
@@ -23,30 +48,37 @@ const ListLink = ({ to, children }) =>
     </HeaderLink>
   </li>
 
-  export default ({ children, data }) =>
-  <div style={{
-    margin: `0 auto`,
-    maxWidth: 750,
-    padding: `2rem 1rem`
+export default ({ children, data }) =>
+<div style={{
+  margin: `0 auto`,
+  maxWidth: 750,
+  padding: `2rem 1rem`
+}}>
+  <header style={{
+    marginBottom: `1.5rem`
   }}>
-    <header style={{
-      marginBottom: `1.5rem`
+    <Link to="/" style={{
+      textShadow: `none`,
+      backgroundImage: `none`,
     }}>
-      <Link to="/" style={{
-        textShadow: `none`,
-        backgroundImage: `none`,
-      }}>
-        <h3 style={{ display: `inline` }}>{data.site.siteMetadata.title}</h3>
-      </Link>
-      <ul style={{ listStyle: `none`, float: `right` }}>
-        <ListLink to="/">Home</ListLink>
-        <ListLink to="/about/">About</ListLink>
-        <ListLink to="/files/">Files</ListLink>
-        <ListLink to="/counter/">Counter</ListLink>
-      </ul>
-    </header>
-    {children()}
-  </div>
+      <h3 style={{ display: `inline` }}>{data.site.siteMetadata.title}</h3>
+    </Link>
+    <ul style={{ listStyle: `none`, float: `right` }}>
+      <ListLink to="/">Home</ListLink>
+      <ListLink to="/about/">About</ListLink>
+      <ListLink to="/files/">Files</ListLink>
+      <ListLink to="/counter/">Counter</ListLink>
+    </ul>
+  </header>
+  {children()}
+  <Footer>
+    <FooterItem>HI</FooterItem>
+    <FooterItem>HI</FooterItem>
+    <FooterItem>HI</FooterItem>
+    <FooterItem>HI</FooterItem>
+    <FooterItem>HI</FooterItem>
+  </Footer>
+</div>
 
 export const query = graphql`
   query LayoutQuery {
