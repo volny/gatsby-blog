7d614b7186796fa59349ecf21430e53ee3936a8b
Felix Volny <felix@volny.co>
2017-10-02 16:28:05 +0700

    images working 👯‍♂️

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,8 +20,20 @@ module.exports = {
               classPrefix: 'language-',
             },
           },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+              // Remove the default behavior of adding a link to each
+              // image.
+              linkImagesToOriginal: false,
+            },
+          },
         ]
-      }
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
