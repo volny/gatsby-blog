5d146503e2fd5a8ae4c1458a8305e84a7f35d400
Felix Volny <felix@volny.co>
2017-10-02 17:14:55 +0700

    footer component

---
+++ src/layouts/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { WebsiteIcon, TwitterIcon, LinkedinIcon, GithubIcon, EmailIcon } from './icons.js'
+import './footer.css'
+
+export default ({children}) => (
+  <footer>
+    <a className="icon-link" href="https://www.felixvolny.com" target="_blank">
+      <WebsiteIcon />
+    </a>
+    <a className="icon-link" href="https://twitter.com/volnyco" target="_blank">
+      <TwitterIcon />
+    </a>
+    <a className="icon-link" href="https://linkedin.com/felixvolny" target="_blank">
+      <LinkedinIcon />
+    </a>
+    <a className="icon-link" href="https://githbub.com/volny" target="_blank">
+      <GithubIcon />
+    </a>
+    <a className="icon-link" href="mailto:website@felixvolny.com">
+      <EmailIcon />
+    </a>
+  </footer>
+)
