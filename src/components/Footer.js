7a77fc2f4ac6794f4c5868715f6afcdc7ddd4de5
Felix Volny <felix@volny.co>
2017-10-04 21:12:58 +0700

    sketch'ed icons 😎

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,26 +6,28 @@ import './footer.css'
 export default class Footer extends Component {
   render() {
     const { height, annoying } = this.props
-    const _height = {height: `${height}px`,}
+    const _height = {}
     const _styles = {...height, position: 'fixed', bottom: 0, left: 0,}
 
     return (
       <footer style={annoying ? _styles : _height}>
+      <div className="icon-container">
         <a className="icon-link" href="https:www.example.com" target="_blank">
           <WebsiteIcon />
-        </a>
-        <a className="icon-link" href="https://twitter.com" target="_blank">
-          <TwitterIcon />
-        </a>
-        <a className="icon-link" href="https://www.linkedin.com" target="_blank">
-          <LinkedinIcon />
-        </a>
-        <a className="icon-link" href="https://github.com" target="_blank">
-          <GithubIcon />
-        </a>
-        <a className="icon-link" href="mailto:someguy@gmail.com">
-          <EmailIcon />
-        </a>
+      </a>
+      <a className="icon-link" href="https://twitter.com" target="_blank">
+        <TwitterIcon />
+      </a>
+      <a className="icon-link" href="https://www.linkedin.com" target="_blank">
+        <LinkedinIcon />
+      </a>
+      <a className="icon-link" href="https://github.com" target="_blank">
+        <GithubIcon />
+      </a>
+      <a className="email-link" href="mailto:me@me.me">
+        <EmailIcon />
+      </a>
+      </div>
       </footer>
     )
   }
