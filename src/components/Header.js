19ca67aa1cd73856c7a1c47eedb965b0fad8b54d
Felix Volny <felix@volny.co>
2017-10-04 15:16:22 +0700

    header is a component

---
+++ src/components/Header.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import styled from "styled-components"
+
+import Logo from './logo.png'
+
+const HeaderLink = styled(Link)`
+  background-Image: none;
+  &:hover {
+    background-image: linear-gradient(to top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0) 1px, #1ca086 1px, #1ca086 2px, rgba(0, 0, 0, 0) 2px);
+  }
+`
+
+const LogoImage = styled.img`
+  display: inline;
+  width: 100px;
+  @media (max-width: 750px) {
+    margin: 0 0 0 1rem;
+  }
+`
+
+const Navigation = styled.ul`
+  listStyle: none;
+  float: right;
+  padding: 30px 0 0 0;
+  @media (max-width: 750px) {
+    margin: 0 1rem 0 0;
+  }
+`
+
+const ListLink = ({ to, children }) =>
+  <li style={{
+    display: `inline-block`,
+    marginRight: `1rem`,
+  }}>
+    <HeaderLink
+      to={to}>
+      {children}
+    </HeaderLink>
+  </li>
+
+export default ({data}) => (
+  <header>
+    <Link to="/" style={{
+      textShadow: `none`,
+        backgroundImage: `none`,
+    }}>
+    <LogoImage src={Logo} />
+    </Link>
+    <Navigation>
+      <ListLink to="/">Home</ListLink>
+      <ListLink to="/about/">About</ListLink>
+    </Navigation>
+  </header>
+)
+
+export const query = graphql`
+  query HeaderQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,75 +1,30 @@
 import React from 'react'
-import Link from 'gatsby-link'
 import styled from "styled-components"
 
 import '../styling/prism-theme.css'
+
 import Footer from '../components/Footer'
-import Logo from './logo.png'
+import Header from '../components/Header'
 
-const HeaderLink = styled(Link)`
-  background-Image: none;
-  &:hover {
-    background-image: linear-gradient(to top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0) 1px, #1ca086 1px, #1ca086 2px, rgba(0, 0, 0, 0) 2px);
-  }
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 750px;
+  padding: 2rem 1rem;
 `
-
-const LogoImage = styled.img`
-  display: inline;
-  width: 100px;
-  @media (max-width: 750px) {
-    margin: 0 0 0 1rem;
-  }
-`
-
-const Navigation = styled.ul`
-  listStyle: none;
-  float: right;
-  padding: 30px 0 0 0;
-  @media (max-width: 750px) {
-    margin: 0 1rem 0 0;
-  }
-`
-
-const ListLink = ({ to, children }) =>
-  <li style={{
-    display: `inline-block`,
-    marginRight: `1rem`,
-  }}>
-    <HeaderLink
-      to={to}>
-      {children}
-    </HeaderLink>
-  </li>
 
 export default ({ children, data }) =>
-  <div style={{
-    margin: `0 auto`,
-    maxWidth: 750,
-    padding: `2rem 1rem`
-  }}>
-
-    <header>
-      <Link to="/" style={{
-        textShadow: `none`,
-        backgroundImage: `none`,
-      }}>
-        <LogoImage src={Logo} alt={data.site.siteMetadata.title} />
-      </Link>
-      <Navigation>
-        <ListLink to="/">Home</ListLink>
-        <ListLink to="/about/">About</ListLink>
-      </Navigation>
-    </header>
-
-    <div style={{padding: `0 0 ${data.site.siteMetadata.footerHeight}px 0`}}>{children()}</div>
+  <Container>
+    <Header />
+    <div style={{ padding: `0 0 ${data.site.siteMetadata.footerHeight}px 0`}}>
+      {children()}
+    </div>
     <Footer height={data.site.siteMetadata.footerHeight} />
-  </div>
+  </Container>
 
 export const query = graphql`
   query IndexLayoutQuery {
     site {
       siteMetadata {
-        title
         footerHeight
       }
     }
