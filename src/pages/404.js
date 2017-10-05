d3c8b02b07cc1ac069ddf78ae33db92304b747b6
Felix Volny <felix@volny.co>
2017-10-05 09:59:45 +0700

    404 page

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+export default () =>
+  <div style={{margin: `-1.5rem 0 0 0`}}>
+    <h1>Sorry, can't find that page.</h1>
+    <Link to="/">Back Home</Link>
+  </div>
+
